e02587857ac0927aa115c2aa30536571e014f56f
usuario <usuario@10.22.146.87>
2016-04-26 12:33:38 +0100

    Falta terminar las condiciones del if de dentro del bucle while y pruebas y el main

--- NumeroPrimo/src/numeroprimo/AlgAleatorio.java
+++ NumeroPrimo/src/numeroprimo/AlgAleatorio.java
@@ -2,15 +2,36 @@
 package numeroprimo;
 
 import java.math.BigInteger;
+import java.util.ArrayList;
 import java.util.HashSet;
+import java.util.Iterator;
 import java.util.Random;
 import java.util.Set;
 
 public class AlgAleatorio {
     
     public boolean esPrimo(BigInteger n, int m){
+        
+        BigInteger bi, mod, uno;
+        uno = new BigInteger("1");
+        
         Set<BigInteger> b = seleccionarMEnteros(n,m);
-        return false;
+        ArrayList k = buscarJ(n,uno);      
+        
+        Iterator<BigInteger> iter = b.iterator();
+        //Preguntar si se pude poner la primera condicion del segundo if fuera del while o en la propia condicion del while
+        while(iter.hasNext()){
+            bi = iter.next();
+            mod =bi.modPow(n.subtract(uno), n);
+            mod = mod.mod(n);
+            if(mod.compareTo(uno) != 0){
+                return false;
+            } 
+            if((k.size() >= 0) && encontrarDividor(bi,n)){
+                return false;
+            }
+        }
+        return true;
     }
        
     public static Set<BigInteger> seleccionarMEnteros(BigInteger n, int m) {
@@ -29,4 +50,29 @@ public class AlgAleatorio {
         }
         return b;
     }
+
+    private ArrayList buscarJ(BigInteger n, BigInteger uno) {
+        ArrayList<BigInteger> solucion = new ArrayList();
+        int j = 1; 
+        BigInteger num = n.subtract(uno);
+        BigInteger dos = new BigInteger("2");
+        BigInteger cero = new BigInteger("0");
+        BigInteger den;
+        BigInteger[] division;
+        while(num.compareTo(dos.pow(j)) >= 0){
+            den = dos.pow(j);
+            division = num.divideAndRemainder(den);
+            if(division[1].compareTo(cero) == 0){
+                solucion.add(division[0]);
+            }
+            j++;
+        }
+        return solucion;
+    }
+
+    private boolean encontrarDividor(BigInteger bi, BigInteger n) {
+        return false;
+    }
+
+    
 }
